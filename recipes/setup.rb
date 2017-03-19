d305f85aa974cbd3d9868f50f855764249f4f70b
vagrant <vagrant@localhost.localdomain>
2017-03-19 00:40:23 +0000

    First Chef Commit on Git

---
+++ recipes/setup.rb
@@ -0,0 +1,15 @@
+package 'vim-enhanced'
+
+package 'tree' do
+	action :install
+end
+
+package 'git' do
+	action :install
+end 
+
+file '/etc/motd' do
+	content 'This Server is the Property of Chef'
+	action :create
+end
+
